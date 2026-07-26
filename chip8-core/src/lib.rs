36eeb7ca626b95a38e440793a2dab04c355d2eee
an0n7mdoy <sadukovs@inbox.ru>
2026-07-26 20:12:45 +0100

    Added load func and load test

--- chip8-core/src/lib.rs
+++ chip8-core/src/lib.rs
@@ -11,9 +11,28 @@ struct Hardware {
     keypad: [bool; 16],
 }
 
+const FONT_SET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 impl Hardware {
     fn new() -> Self {
-        Self {
+        let mut machine = Self {
             memory: [0; 4096],
             registers: [0; 16],
             index: 0,
@@ -24,7 +43,14 @@ impl Hardware {
             s_timer: 0,
             display: [[false; 32]; 64],
             keypad: [false; 16],
-        }
+        };
+        machine.memory[0x050..0x0A0].copy_from_slice(&FONT_SET);
+        machine
+    }
+
+    fn load(&mut self, rom: &[u8]) {
+        let end: usize = 0x200 + rom.len();
+        self.memory[0x200..end].copy_from_slice(rom);
     }
 }
 
@@ -35,15 +61,24 @@ mod tests {
     #[test]
     fn it_works() {
         let machine = Hardware::new();
-        assert_eq!(machine.memory, [0; 4096]);
-        assert_eq!(machine.registers, [0; 16]);
-        assert_eq!(machine.stack, [0; 16]);
-        assert_eq!(machine.display, [[false; 32]; 64]);
-        assert_eq!(machine.keypad, [false; 16]);
+        assert_eq!(machine.memory.len(), 4096);
+        assert_eq!(machine.registers.len(), 16);
+        assert_eq!(machine.stack.len(), 16);
+        assert_eq!(machine.display.len(), 64);
+        assert_eq!(machine.display[0].len(), 32);
+        assert_eq!(machine.keypad.len(), 16);
         assert_eq!(machine.index, 0);
         assert_eq!(machine.stack_p, 0);
         assert_eq!(machine.d_timer, 0);
         assert_eq!(machine.s_timer, 0);
         assert_eq!(machine.pc, 0x200)
+    }
+
+    #[test]
+    fn memory_load() {
+        let mut machine = Hardware::new();
+        let that = [0x12u8, 0x34u8, 0xABu8];
+        machine.load(&that);
+        assert_eq!(machine.memory[0x200..0x203], that);
     }
 }
